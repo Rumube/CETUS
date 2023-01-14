f2f26a1bcb2567334eab06e66747e52d422532f2
Rumube <rumube@floridauniversitaria.es>
2023-01-14 20:06:39 +0100

    Path Controller
    Enter and exit added, direction and creation.

---
+++ Assets/Scripts/Paths/PathController.cs
@@ -0,0 +1,31 @@
+using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    //Values
+    private float _distanceTravelled = 0;
+    [Header("References")]
+    //References
+    public GameObject _guide;
+    private GameObject _player;
+    private PathCreator _pathcreator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _pathcreator = GetComponent<PathCreator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distanceTravelled = _pathcreator.path.GetClosestDistanceAlongPath(_player.transform.position);
+        _guide.transform.position = _pathcreator.path.GetPointAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+        _guide.transform.rotation = _pathcreator.path.GetRotationAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+    }
+}
--- Assets/Scripts/Paths/WhalePahtController.cs
+++ Assets/Scripts/Paths/WhalePahtController.cs
@@ -7,34 +7,136 @@ public class WhalePahtController : MonoBehaviour
 {
     [Header("Status")]
     private bool _isPath = false;
+    [SerializeField]
     private bool _direction = true;
 
     [Header("Travel values")]
-    public PathCreator _pathcreator;
-    public float _speed = 1;
-    float _distanceTravelled;
-    public const float MINSPEED = 30;
+    public float _speed = 0;
+    float _distanceTravelled = 0;
+    private const float MINSPEED = 5f;
+
+    private PathCreator _pathcreator;
+
+    //Input Values
+    public float _exitTime = 0;
+    public float _timeToNextTravel = 0;
+
+    private float _verAxis = 0;
+    private float _horAxis = 0;
+    private float _nextExit = 0;
+    private float _nextTravel = 0;
+    private bool _isExit = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.C)) {
-            _isPath = !_isPath;
-            _distanceTravelled = _pathcreator.path.GetClosestDistanceAlongPath(transform.position);
-        }
+        UpdateInputs();
+        ExitConfiguration();
+        UpdateInPath();
+    }
+    /// <summary>
+    /// Manage the inputs
+    /// </summary>
+    private void UpdateInputs()
+    {
+        _verAxis = Input.GetAxis("Vertical");
+        _horAxis = Input.GetAxis("Horizontal");
+    }
 
+    /// <summary>
+    /// Use the <see cref="_horAxis"/> and <see cref="_exitTime"/>
+    /// values to check if the player want to leave the path.
+    /// </summary>
+    private void ExitConfiguration()
+    {
+        if (_horAxis >= 0.5f || _horAxis <= -0.5f)
+        {
+            if (!_isExit)
+            {
+                _isExit = true;
+                _nextExit = Time.realtimeSinceStartup + _exitTime;
+            }else if(Time.realtimeSinceStartup >= _nextExit)
+            {
+                _isExit = false;
+                _isPath = false;
+                _nextTravel = Time.realtimeSinceStartup + _timeToNextTravel;
+            }
+        }
+        else
+        {
+            _isExit = false;
+        }
+    }
+
+    /// <summary>
+    /// Manage the movement in path
+    /// </summary>
+    private void UpdateInPath()
+    {
         if (_isPath)
         {
-            float playerVel = Input.GetAxis("Vertical");
+            float playerVel = _verAxis;
+            SetDirection(playerVel);
+            playerVel = CorrectVelocity(playerVel);
             _distanceTravelled += (playerVel * _speed) + (MINSPEED * Time.deltaTime);
             transform.position = _pathcreator.path.GetPointAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
             transform.rotation = _pathcreator.path.GetRotationAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+        }
+
+    }
+
+    /// <summary>
+    /// Set the player direcction, if the vertical axis is
+    /// less than -0.5f <see cref="_direction"/> is FALSE and if the vertical axis is
+    /// gratter than 0.5f <see cref="_direction"/> is TRUE
+    /// </summary>
+    /// <param name="playerVel"></param>
+    private void SetDirection(float playerVel)
+    {
+        if (playerVel <= -0.5f)
+        {
+            _direction = false;
+        }
+        else if (playerVel >= 0.5f)
+        {
+            _direction = true;
+        }
+    }
+    /// <summary>
+    /// Force the variable <paramref name="playerVel"/> to set
+    /// different than 0 to move always the player.
+    /// </summary>
+    /// <param name="playerVel"></param>
+    /// <returns></returns>
+    private float CorrectVelocity(float playerVel)
+    {
+        if (_direction && playerVel == 0)
+        {
+            playerVel = 0.1f;
+        }
+        else if (!_direction && playerVel == 0)
+        {
+            playerVel = -0.1f;
+        }
+        return playerVel;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PathGuide" && !_isPath)
+        {
+            if(Time.realtimeSinceStartup >= _nextTravel)
+            {
+                _isPath = true;
+                _pathcreator = other.gameObject.GetComponentInParent<PathCreator>();
+                _distanceTravelled = _pathcreator.path.GetClosestDistanceAlongPath(transform.position);
+            }
         }
     }
 }
