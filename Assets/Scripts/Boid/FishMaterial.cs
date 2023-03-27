cae9df3c57f83bcb95f3b62e9fa0797a23b4c922
Rumube <rumube@floridauniversitaria.es>
2023-03-27 20:10:43 +0200

    Update shaders

--- Assets/Scripts/Boid/FishMaterial.cs
+++ Assets/Scripts/Boid/FishMaterial.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class FishMaterial : MonoBehaviour
 {
+    [SerializeField] Texture2D[] _posibleTextures;
+    [SerializeField][ColorUsage(true, true, 0f, 8f, 0.125f, 3f)][System.Obsolete] Color[] _posibleColors;
     private Material _material;
 
     private void Awake()
     {
         _material = GetComponent<Renderer>().material;
         SetFuerza(0.4f);
+        int random = Random.Range(0,_posibleTextures.Length);
+        _material.SetTexture("_Textura", _posibleTextures[random]);
+        _material.SetColor("_Color", _posibleColors[random]);
     }
 
     public void SetFuerza(float fuerza)
