5d2d0055fe943016058401977ec765ac2ea134bc
Rumube <rumube@floridauniversitaria.es>
2023-05-08 11:50:55 +0200

    Programación animaciones

--- Assets/Scripts/Nexo/AnimationNexo.cs
+++ Assets/Scripts/Nexo/AnimationNexo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class AnimationNexo : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Animator[] _memorysAnims;
+
     private bool _showingMemory = false;
 
     private void Awake()
@@ -20,6 +23,14 @@ public class AnimationNexo : MonoBehaviour
         _showingMemory = true;
         _animator.Play("Nexo_Animation01_Zone0");
     }
+
+    public IEnumerator StartAnimClose()
+    {
+        //TODO: ACTIVAR ANIM
+        yield return new WaitForSeconds(2f);
+        _showingMemory = false;
+
+    }
     public void StartAnimBeat()
     {
         if (!_showingMemory)
@@ -28,5 +39,41 @@ public class AnimationNexo : MonoBehaviour
         }
     }
 
+    public IEnumerator StartMemory1()
+    {
+        StartAnimOpen();
+        yield return new WaitForSeconds(2);
+        _memorysAnims[0].Play("Mountain_Animation_Story1");
+        yield return new WaitForSeconds(2);
+        StartCoroutine(StartAnimClose());
+    }
 
+    public IEnumerator StartMemory2()
+    {
+        StartAnimOpen();
+        yield return new WaitForSeconds(2);
+        _memorysAnims[1].Play("Constelation_Animation_Story5");
+        yield return new WaitForSeconds(0.2f);
+        _memorysAnims[2].Play("RoomAssets_Animation_Story5");
+        yield return new WaitForSeconds(2);
+        StartCoroutine(StartAnimClose());
+    }
+
+    public IEnumerator StartMemory3()
+    {
+        StartAnimOpen();
+        yield return new WaitForSeconds(2);
+        _memorysAnims[3].Play("Gradas_Animation_Story3");
+        yield return new WaitForSeconds(2);
+        StartCoroutine(StartAnimClose());
+    }
+
+    public IEnumerator StartMemory4()
+    {
+        StartAnimOpen();
+        yield return new WaitForSeconds(2);
+        _memorysAnims[4].Play("Volcan_Animation_Story10");
+        yield return new WaitForSeconds(2);
+        StartCoroutine(StartAnimClose());
+    }
 }
