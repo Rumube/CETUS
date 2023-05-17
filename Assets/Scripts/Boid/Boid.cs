0312ca4cee1f0a00b7f2c17608af9d6b3226251a
Rumube <rumube@floridauniversitaria.es>
2023-05-17 22:59:31 +0200

    Void capado

--- Assets/Scripts/Boid/Boid.cs
+++ Assets/Scripts/Boid/Boid.cs
@@ -91,91 +91,93 @@ public class Boid : MonoBehaviour
 
     public void UpdateBoid(Transform Spawner)
     {
-        if (Time.realtimeSinceStartup >= _timeRunaway)
+        if (Vector3.Distance(_player.transform.position, transform.position) <= 1000)
         {
-           acceleration = Vector3.zero;
-            _isScared = PlayerDetection();
-            if (target != null)
+            if (Time.realtimeSinceStartup >= _timeRunaway)
             {
-                Vector3 offsetToTarget = (target.position - position);
-                acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
-            }
-            
-            if (numPerceivedFlockmates != 0)
-            {
-                centreOfFlockmates /= numPerceivedFlockmates;
-
-                Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
-
-                var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
-                var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
-                var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight*10;
-
-                acceleration += alignmentForce;
-                acceleration += cohesionForce;
-                acceleration += seperationForce;
-            }
-            if (OutofRadious())
-            {
-                Vector3 direction = (_spawner.transform.position - position).normalized;
-                Vector3 collisionAvoidForce = SteerTowards(direction) * settings.avoidCollisionWeight;
-                acceleration += collisionAvoidForce;
-            }
-            else if (action == behaviour.Follower)
-            {
-                
-                Vector3 direction = (_player.transform.position - position).normalized;
-
-                Vector3 cohesionWeight = SteerTowards(direction) * settings.cohesionWeight;
-                acceleration += cohesionWeight*10;
-            }
-             else if (_isScared)
-            {
-               Vector3 direction = (position - _player.transform.position).normalized;
-               _lastDirect = direction;
-               Vector3 collisionAvoidForce = SteerTowards(direction) * settings.avoidCollisionWeight;
-               acceleration += collisionAvoidForce * 10;
-               
-            }
-            else if (IsHeadingForCollision())
-            {
-                Vector3 collisionAvoidDir = ObstacleRays();
-                Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
-                acceleration += collisionAvoidForce;
-            }
-            
-
-            velocity += acceleration * Time.deltaTime;
-            float speed = velocity.magnitude;
-            Vector3 dir = velocity / speed;
-            
-            if (action == behaviour.Follower)
-            {
-
-                if (Vector3.Distance(position, _player.transform.position) <= 20)
+                acceleration = Vector3.zero;
+                _isScared = PlayerDetection();
+                if (target != null)
                 {
-                    speed = Mathf.Clamp(speed, 50, 60);
+                    Vector3 offsetToTarget = (target.position - position);
+                    acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+                }
+
+                if (numPerceivedFlockmates != 0)
+                {
+                    centreOfFlockmates /= numPerceivedFlockmates;
+
+                    Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
+
+                    var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
+                    var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
+                    var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight * 10;
+
+                    acceleration += alignmentForce;
+                    acceleration += cohesionForce;
+                    acceleration += seperationForce;
+                }
+                if (OutofRadious())
+                {
+                    Vector3 direction = (_spawner.transform.position - position).normalized;
+                    Vector3 collisionAvoidForce = SteerTowards(direction) * settings.avoidCollisionWeight;
+                    acceleration += collisionAvoidForce;
+                }
+                else if (action == behaviour.Follower)
+                {
+
+                    Vector3 direction = (_player.transform.position - position).normalized;
+
+                    Vector3 cohesionWeight = SteerTowards(direction) * settings.cohesionWeight;
+                    acceleration += cohesionWeight * 10;
+                }
+                else if (_isScared)
+                {
+                    Vector3 direction = (position - _player.transform.position).normalized;
+                    _lastDirect = direction;
+                    Vector3 collisionAvoidForce = SteerTowards(direction) * settings.avoidCollisionWeight;
+                    acceleration += collisionAvoidForce * 10;
+
+                }
+                else if (IsHeadingForCollision())
+                {
+                    Vector3 collisionAvoidDir = ObstacleRays();
+                    Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
+                    acceleration += collisionAvoidForce;
+                }
+
+
+                velocity += acceleration * Time.deltaTime;
+                float speed = velocity.magnitude;
+                Vector3 dir = velocity / speed;
+
+                if (action == behaviour.Follower)
+                {
+
+                    if (Vector3.Distance(position, _player.transform.position) <= 20)
+                    {
+                        speed = Mathf.Clamp(speed, 50, 60);
+                    }
+                    else
+                    {
+                        speed = Mathf.Clamp(speed, 60, 100);
+                    }
                 }
                 else
                 {
-                    speed = Mathf.Clamp(speed, 60, 100);
+                    speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
                 }
+                velocity = dir * speed;
+                cachedTransform.position += velocity * Time.deltaTime;
+                cachedTransform.forward = dir;
+                position = cachedTransform.position;
+                forward = dir;
             }
             else
             {
-                speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed); 
+                transform.position += transform.forward * 0.5f;
             }
-            velocity = dir * speed;
-            cachedTransform.position += velocity * Time.deltaTime;
-            cachedTransform.forward = dir;
-            position = cachedTransform.position;
-            forward = dir;
         }
-        else
-        {
-            transform.position += transform.forward * 0.5f;
-        }
-
     }
     /// <summary>
     /// Checks if the boid is in or out of the spawn area
